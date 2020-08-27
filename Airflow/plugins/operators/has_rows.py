245c4d15ebb96bf347b6a506d797b492615bfc05
Alexander Scherf <alexander-scherf@gmx.net>
2020-08-27 11:49:50 +0200

    update

--- Airflow/plugins/operators/has_rows.py
+++ Airflow/plugins/operators/has_rows.py
@@ -2,6 +2,7 @@ import logging
 
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
+# all operators inherit from the baseoperator
 from airflow.utils.decorators import apply_defaults
 
 
@@ -18,11 +19,16 @@ class HasRowsOperator(BaseOperator):
         self.redshift_conn_id = redshift_conn_id
 
     def execute(self, context):
+        # every operator has an execute function
         redshift_hook = PostgresHook(self.redshift_conn_id)
-        records = redshift_hook.get_records(f"SELECT COUNT(*) FROM {self.table}")
+        records = redshift_hook.get_records(
+            f"SELECT COUNT(*) FROM {self.table}")
         if len(records) < 1 or len(records[0]) < 1:
-            raise ValueError(f"Data quality check failed. {self.table} returned no results")
+            raise ValueError(
+                f"Data quality check failed. {self.table} returned no results")
         num_records = records[0][0]
         if num_records < 1:
-            raise ValueError(f"Data quality check failed. {self.table} contained 0 rows")
-        logging.info(f"Data quality on table {self.table} check passed with {records[0][0]} records")
+            raise ValueError(
+                f"Data quality check failed. {self.table} contained 0 rows")
+        logging.info(
+            f"Data quality on table {self.table} check passed with {records[0][0]} records")
